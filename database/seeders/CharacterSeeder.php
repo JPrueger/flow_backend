6434af638b30d55725497dd977cd5eb5ff94cc2a
Jeannine Prueger <jeannine.prueger@gmail.com>
2021-11-07 23:03:22 +0100

    feat: add character seeder

---
+++ database/seeders/CharacterSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Character;
+use Illuminate\Database\Seeder;
+
+class CharacterSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Character::factory()->count(10)->create();
+    }
+}
