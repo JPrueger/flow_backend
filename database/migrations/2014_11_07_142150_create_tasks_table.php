e40945fd4b60dc9356fa425dfd37f1d84ecf06b0
Jeannine Prueger <jeannine.prueger@gmail.com>
2021-11-07 22:54:36 +0100

    feat: add task migration

---
+++ database/migrations/2014_11_07_142150_create_tasks_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('project_id')->nullable(); 
+            $table->string('title'); 
+            $table->string('description'); 
+
+            $table->unsignedBigInteger('assigne_id');
+            $table->foreign('assigne_id')->references('id')->on('users');
+
+            $table->integer('storypoints');
+            $table->string('status');
+            $table->integer('sort_index');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
