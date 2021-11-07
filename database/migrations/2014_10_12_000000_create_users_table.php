5c2d740ed4b5e418703fbdefda96cf648e0a0897
Jeannine Prueger <jeannine.prueger@gmail.com>
2021-11-07 23:06:10 +0100

    refactor: adjust users table with relations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,17 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->timestamps();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            
+            $table->unsignedBigInteger('character_id');
+            $table->foreign('character_id')->references('id')->on('characters');
+
+            $table->integer('storypoints');
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
-            $table->timestamps();
         });
     }
 
