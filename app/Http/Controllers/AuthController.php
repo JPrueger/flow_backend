c2d78d14de31f5361fcb0fa5e0d9c815435b2a70
Jeannine Prueger <jeannine.prueger@gmail.com>
2021-11-07 13:04:36 +0100

    refactor: make email when registering unique

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -36,7 +36,7 @@ class AuthController extends Controller
     {
         $this->validate($request, [
             'name' => 'required',
-            'email' => 'required|email',
+            'email' => 'required|email|unique:users',
             'password' => 'required'
         ]);
 
