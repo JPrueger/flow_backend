180d4ae912cbe80daa861409638db050fb51380e
Jeannine Prueger <jeannine.prueger@gmail.com>
2021-11-07 22:58:49 +0100

    feat: add characterlevel migration

---
+++ database/migrations/2012_11_07_142346_create_characterlevels_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCharacterlevelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('characterlevels', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('type_id')->nullable();
+            $table->integer('points_upgrade');
+            $table->text('video_state');
+            $table->text('video_upgrade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('characterlevels');
+    }
+}
