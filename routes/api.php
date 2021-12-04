a181d20e753d25133c8e4face81f4c53e1fd3865
kerstin <kerstin.wukoschitz@gmx.net>
2021-12-04 12:30:03 +0100

    remove cors in routing

--- routes/api.php
+++ routes/api.php
@@ -22,16 +22,16 @@ Route::get('/todos', [App\Http\Controllers\TodosController::class, 'index']);
 Route::post('/todos', [App\Http\Controllers\TodosController::class, 'store']);
 
 // register user
-Route::post('/user/register', [App\Http\Controllers\AuthController::class, 'register'])->middleware('cors');
+Route::post('/user/register', [App\Http\Controllers\AuthController::class, 'register']);
 
 // login user
 Route::post('/user/login', [App\Http\Controllers\AuthController::class, 'login']);
 
 // store Project
-Route::post('/add-project/create', [App\Http\Controllers\ProjectController::class, 'store'])->middleware('cors');
+Route::post('/add-project/create', [App\Http\Controllers\ProjectController::class, 'store']);
 
 // sorting tasks
-Route::post('/sort-task', [App\Http\Controllers\ProjectController::class, 'sort'])->middleware('cors');
+Route::post('/sort-task', [App\Http\Controllers\ProjectController::class, 'sort']);
 
 // get all tasks
 Route::get('/tasks/index/{id}', [App\Http\Controllers\TaskController::class, 'index']);
