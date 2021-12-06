33502a0b9f478576882e55a7c7c159906169e2d1
Jeannine Prueger <jeannine.prueger@gmail.com>
2021-12-06 20:51:31 +0100

    feat: get all users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,12 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\User;
 
-class UserController
+class UserController extends Controller
 {
     public function getAllUsers() {
-        $users = User::all();
-
+        $users = User::all(['id', 'name', 'email']);
         return response()->json($users);
     }
 }
